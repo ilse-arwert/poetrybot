0631e766ec31bae4b41c2b2e7f7edab4b109019f
Ilse Arwert <ilsearwert@gmail.com>
2021-11-01 11:51:35 +0100

    got rid of old addState

--- markovChain.js
+++ markovChain.js
@@ -5,13 +5,10 @@ const json = require('./data.json')
 // If you are generating words, adding multiple states
 markov.addStates(json.data);
 
-  // If you are generating text, adding a single state
-markov.addStates('The weather for tomorrow might be sunny');
-
 markov.train();
 
 // var longText = markov.generate(50);
-var text = markov.generateRandom(50);
+var text = markov.generateRandom(150);
 // var possibilities = markov.getPossibilities('Hey');
 
 console.log(text);
